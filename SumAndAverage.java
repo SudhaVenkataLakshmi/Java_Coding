d06b9a76b1618adea529631e0ee4bc9ffe4c7a56
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-02 20:57:15 +0530

    sum and avg of array

---
+++ SumAndAverage.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumAndAverage {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any Number: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter "+n+ " Numbers: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+       int sum = 0;
+       for(int num : arr){
+        sum += num;
+       }
+       double average = (double) sum / n;
+       System.out.println("Sum of Elements = " + sum);
+       System.out.println("Average of Elements = " + average);
+       sc.close();
+    }
+}
