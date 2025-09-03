5e126975241dadb018e9ac9f8f7f596519c3461c
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-03 14:36:46 +0530

    new change on Even or Odd problem

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner Scanner = new Scanner(System.in);
         System.out.print("Enter any Value: ");
-        double val = scanner.nextDouble();
+        double val = Scanner.nextDouble();
         if(val%2 == 0){
             System.out.println("Your value is Even.");
         }else{
