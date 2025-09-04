8ec133171ae19c28d51856a4877e65dd114f04ed
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-04 15:22:44 +0530

    Merge the Sorted Arrays

---
+++ MergeSortedArrays.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static void main(String [] args){
+        int arr1 [] = {1,2,4,6,9};
+        int arr2 [] = {3,5,7,10};
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int merged [] = new int[n1 + n2];
+        int i = 0, j = 0, k = 0;
+        while (i < n1 ) {
+            merged [k++] = arr1[i++];
+        }
+        while (j < n2) {
+            merged[k++] = arr2[j++];
+        }
+
+        System.out.println("Merged Sorted Array: "+Arrays.toString(merged));
+
+    }
+}
