67288161a7c486b06a9b9be58537485a9a04e682
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-09 19:25:38 +0530

    Finds the square of number

--- N_Square.java
+++ N_Square.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class N_Square {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter n value: ");
+        System.out.print("Enter any value: ");
         int n = scanner.nextInt();
         if(n>0){
             int res = n*n;
             System.out.println("Sqauare of " +n+ " is: " +res);
         }else{
-            System.out.println("Please Enter a Positive Number.");
+            System.out.println("Please! Enter a Positive Number.");
         }
         scanner.close();
     }
