33a3fd63266ae8f49f741e8ac9f32ae0852df4bc
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-09 19:32:09 +0530

    Sorting Arrays

--- SortArray.java
+++ SortArray.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 
 public class SortArray{
     public static void main(String [] args){
-        Integer arr[] = {2,5,8,1,3,7};
+        Integer arr[] = {2,1,3,12,8,7,10,9};
         System.out.println("Original Array: " +Arrays.toString(arr));
         Arrays.sort(arr);
         System.out.println("Ascending Order: " + Arrays.toString(arr));
