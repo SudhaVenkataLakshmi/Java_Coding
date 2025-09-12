5a32adc8dab3c8727f00a451e49c6b7025956749
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-12 21:07:48 +0530

    Reversing the given string

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ReverseString {
+    public static void main(String [] args){
+        String str = "Hello Java";
+        Stack<Character> stack = new Stack<>();
+        for(char c : str.toCharArray()){
+            stack.push(c);
+        }
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+        System.out.println("Original: " +str);
+        System.out.println("Reversed: " +reversed);
+    }
+}
