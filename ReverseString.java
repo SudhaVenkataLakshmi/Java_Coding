6b9a884acbf8322120a341202f53dcb6efe7f590
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-12 21:32:19 +0530

    Reversing the given string

--- ReverseString.java
+++ ReverseString.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ReverseString {
     public static void main(String [] args){
-        String str = "Hello Java";
+        String str = "Sudha Venkata Lakshmi";
         Stack<Character> stack = new Stack<>();
         for(char c : str.toCharArray()){
             stack.push(c);
