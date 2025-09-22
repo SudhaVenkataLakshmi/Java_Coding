21b697182bd191f0aa7d0cf2eb1a9afcef25fccb
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-22 19:17:24 +0530

    Employee Details

---
+++ EmployeeDemo.java
@@ -0,0 +1,26 @@
+class Employee {
+    int id;
+    String name;
+    double salary;
+
+    Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
+    }
+}
+
+public class EmployeeDemo {
+    public static void main(String[] args) {
+        Employee e1 = new Employee(101, "Sushma", 35000);
+        Employee e2 = new Employee(102, "Ravi", 40000);
+
+        e1.display();
+        e2.display();
+    }
+}
+
