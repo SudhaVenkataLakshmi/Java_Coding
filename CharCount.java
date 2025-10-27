d4fe76eb60bd1a50ebceaca45e226d1d1f2ef9c2
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-27 18:32:12 +0530

    Counts Characters, Digits and Spl Symbols

---
+++ CharCount.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class CharCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        int letters = 0, digits = 0, special = 0;
+        for (char ch : str.toCharArray()) {
+            if (Character.isLetter(ch)) letters++;
+            else if (Character.isDigit(ch)) digits++;
+            else special++;
+        }
+
+        System.out.println("Letters: " + letters);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special characters: " + special);
+    }
+}
