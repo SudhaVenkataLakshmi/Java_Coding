ca18f48007c97c8e4458c11187391b198d30b96d
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-29 21:40:01 +0530

    Sarching Elements

--- BinarySearchEx.java
+++ BinarySearchEx.java
@@ -23,7 +23,7 @@ public class BinarySearchEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int arr[] = {2,6,7,3,9,12,10};
+        int arr[] = {2,6,8,9,0,10,4,3};
         Arrays.sort(arr); 
 
         System.out.println("Sorted Array: " + Arrays.toString(arr));
