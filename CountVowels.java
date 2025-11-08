683804c1648b832ef6eb412e8bbeeaead3b14c1a
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-11-08 15:05:10 +0530

    Count Vowels and Consonants in Given string

--- CountVowels.java
+++ CountVowels.java
@@ -1,6 +1,6 @@
 public class CountVowels {
     public static void main(String[] args) {
-        String str = "Challenge";
+        String str = "Coding";
         str = str.toLowerCase();
 
         int vowels = 0, consonants = 0;
