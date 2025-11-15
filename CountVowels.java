5a3912284b5e4a56e05c4f9dc666450fc85c2621
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-11-15 21:25:20 +0530

    Count Vowels and Consonants

--- CountVowels.java
+++ CountVowels.java
@@ -1,6 +1,6 @@
 public class CountVowels {
     public static void main(String[] args) {
-        String str = "Java Programming";
+        String str = "Programming";
         str = str.toLowerCase();
 
         int vowels = 0, consonants = 0;
