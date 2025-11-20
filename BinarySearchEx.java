e99b5d868992309389725122529316250bd8f4b6
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-11-20 20:40:04 +0530

    Search Element

--- BinarySearchEx.java
+++ BinarySearchEx.java
@@ -23,7 +23,7 @@ public class BinarySearchEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int arr[] = {4,7,9,3,2,1,10};
+        int arr[] = {11,6,8,9,3,10,2,4};
         Arrays.sort(arr); 
 
         System.out.println("Sorted Array: " + Arrays.toString(arr));
