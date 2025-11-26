ab948612de0670ce90dd8ff675de49123d9db4a7
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-11-26 16:59:43 +0530

    Checks 2 arrays are sorted or not

--- CheckSortedArray.java
+++ CheckSortedArray.java
@@ -9,7 +9,7 @@ public class CheckSortedArray {
     }
     public static void main(String[] args) {
         int arr1[] = {22,4,6,8,4,3,23};
-        int arr2[] = {3,5,7,9,03,2,3};
+        int arr2[] = {12,17,30,57,80};
 
         System.out.println("arr1 is sorted? " + isSorted(arr1));
         System.out.println("arr2 is sorted? " + isSorted(arr2));
