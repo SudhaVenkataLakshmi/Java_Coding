3339cdd6f52230938a80dc681ec9f1f847492348
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-01 20:40:50 +0530

    Employee Details

--- EmployeeDemo.java
+++ EmployeeDemo.java
@@ -16,8 +16,8 @@ class Employee {
 
 public class EmployeeDemo {
     public static void main(String[] args) {
-        Employee e1 = new Employee(4001, "Sandhya", 75000);
-        Employee e2 = new Employee(4002, "Sudha", 80000);
+        Employee e1 = new Employee(4001, "Sandhya", 70000);
+        Employee e2 = new Employee(4002, "Sudha", 65000);
 
         e1.display();
         e2.display();
