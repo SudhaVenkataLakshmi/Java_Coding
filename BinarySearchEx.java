e90766d628c6b4b66fb04b80c9e8cc12954273e0
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-01 20:45:42 +0530

    Sarch element in Array

--- BinarySearchEx.java
+++ BinarySearchEx.java
@@ -23,7 +23,7 @@ public class BinarySearchEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int arr[] = {12,5,7,9,10,23};
+        int arr[] = {11,4,6,8,5,10,15,20};
         Arrays.sort(arr); 
 
         System.out.println("Sorted Array: " + Arrays.toString(arr));
