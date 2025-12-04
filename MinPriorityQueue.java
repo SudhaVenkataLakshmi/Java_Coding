827bbffd81986de40d27e17fef143771bcf4ba6c
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-04 18:37:49 +0530

    Minimum Priority Queue

--- MinPriorityQueue.java
+++ MinPriorityQueue.java
@@ -3,9 +3,9 @@ import java.util.PriorityQueue;
 public class MinPriorityQueue {
     public static void main(String [] args){
         PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        minPQ.add(7);
+        minPQ.add(3);
         minPQ.add(5);
-        minPQ.add(2);
-        minPQ.add(8);
         minPQ.add(1);
         minPQ.forEach((Integer val) -> System.out.println(val));
         while(!minPQ.isEmpty()){
