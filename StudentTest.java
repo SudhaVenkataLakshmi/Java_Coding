de1d455d2cae2d77a05bf045a8ea5461220f1ac4
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-05 10:32:53 +0530

    Student Details

--- StudentTest.java
+++ StudentTest.java
@@ -17,8 +17,8 @@ class Student {
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student s1 = new Student("Sushma", 501, 89.9);
-        Student s2 = new Student("Sudha", 502, 76.5);
+        Student s1 = new Student("Sushma", 508, 85.0);
+        Student s2 = new Student("Sandhya", 509, 72.5);
         s1.displayDetails();
         System.out.println();
         s2.displayDetails();
