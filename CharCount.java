e56fd0ddd95aadaa757077cf9a3ba9054952650e
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-05 10:27:27 +0530

    Count Characters, Numbers and Special characters

--- CharCount.java
+++ CharCount.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class CharCount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any String: ");
         String str = sc.nextLine();
 
         int letters = 0, digits = 0, special = 0;
