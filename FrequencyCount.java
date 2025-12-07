106e89fef27dc56377bee1bcec2f38b2f4b75b6a
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-07 19:08:50 +0530

    Counts Frequent numbers

--- FrequencyCount.java
+++ FrequencyCount.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class FrequencyCount {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(3,4,5,4,5,6,4,3,3,2,1,3,4,4,4);
+        List<Integer> numbers = Arrays.asList(3,4,2,2,3,4,5,4,2,6,7,8,5,3,3,2);
 
         Map<Integer, Integer> freqMap = new HashMap<>();
 
