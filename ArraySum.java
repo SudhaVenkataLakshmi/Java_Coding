4f5ae27f21b6682e39b7baf14cea711fe685b9df
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-11 18:13:34 +0530

    Sum of Elements

--- ArraySum.java
+++ ArraySum.java
@@ -7,7 +7,7 @@ public class ArraySum {
         return sum;
     }
     public static void main(String [] args){
-        int [] arr = {11,30,34,7,9,10,3,75};
+        int [] arr = {11,30,34,30,46,31};
         System.out.println("Sum of Elements in an Array: " + calculateSum(arr));
     }
 }
