b9fb05be1422c8d817583298b220e3883c060974
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-16 12:04:05 +0530

    String Occurence

--- String_Occurence.java
+++ String_Occurence.java
@@ -1,8 +1,14 @@
 
+import java.util.Scanner;
 public class String_Occurence {
     public static void main(String[] args){
-        String n = "1231";
-        char d = '1';
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any String: ");
+        String n = sc.nextLine();
+        System.out.println("Enter your digit: ");
+        char d = sc.next().charAt(0);
+
+
         int max = Integer.MIN_VALUE;
         for(int i = 0; i<n.length(); i++){
             if(n.charAt(i) == d){
