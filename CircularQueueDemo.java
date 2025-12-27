a7c4a7cc15886d76241bf25bb35b54585b23b596
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-27 20:05:25 +0530

    Circular Queue Example

--- CircularQueueDemo.java
+++ CircularQueueDemo.java
@@ -37,9 +37,9 @@ class CircularQueue {
 public class CircularQueueDemo {
     public static void main(String[] args) {
         CircularQueue cq = new CircularQueue(5);
-        cq.enqueue(90);
+        cq.enqueue(60);
         cq.enqueue(25);
-        cq.enqueue(35);
+        cq.enqueue(55);
         cq.display(); 
         cq.dequeue();
         cq.display();  
