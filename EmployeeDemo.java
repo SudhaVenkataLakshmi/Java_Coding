e85ab5ed0eca24044703db904d5cdbebefb2234e
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-28 19:29:01 +0530

    Employee Details

--- EmployeeDemo.java
+++ EmployeeDemo.java
@@ -16,8 +16,8 @@ class Employee {
 
 public class EmployeeDemo {
     public static void main(String[] args) {
-        Employee emp1 = new Employee(608, "Devika", 55000);
-        Employee emp2 = new Employee(309, "Bhumika", 85000);
+        Employee emp1 = new Employee(708, "Devika", 35000);
+        Employee emp2 = new Employee(509, "Bhumika", 75000);
 
         emp1.display();
         emp2.display();
