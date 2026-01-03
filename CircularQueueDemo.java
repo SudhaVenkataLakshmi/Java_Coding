8eda96a1a4e8efde1629d90dfc52d78e787ca97b
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-01-03 11:32:19 +0530

    Circular Queue

--- CircularQueueDemo.java
+++ CircularQueueDemo.java
@@ -38,8 +38,8 @@ public class CircularQueueDemo {
     public static void main(String[] args) {
         CircularQueue cq = new CircularQueue(5);
         cq.enqueue(60);
-        cq.enqueue(45);
-        cq.enqueue(75);
+        cq.enqueue(30);
+        cq.enqueue(25);
         cq.display(); 
         cq.dequeue();
         cq.display(); 
