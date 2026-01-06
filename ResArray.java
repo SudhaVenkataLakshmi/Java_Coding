42fe8f7fd9e97964208fa42f121a45df93b3495a
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-01-06 20:00:57 +0530

    Array Operations

--- ResArray.java
+++ ResArray.java
@@ -1,6 +1,6 @@
 public class ResArray {
     public static void main(String[] args){
-        int[] arr = {7,8,5,5,9,2,2,0,1,6,7,3};
+        int[] arr = {7,8,5,5,9,2,2,0,1,6,7,3,4,4,3,2};
         int n = arr.length;
         int max_val = 0;
         for(int start = 0; start < n; start++){
