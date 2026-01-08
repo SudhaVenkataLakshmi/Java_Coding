f68072e30d240231d9f994d3cf3c5d3c20b07995
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-01-08 09:51:23 +0530

    Indexed Array Problem

--- IndexedArray.java
+++ IndexedArray.java
@@ -1,6 +1,6 @@
 public class IndexedArray {
     public static void main(String[] args){
-        int n = 7;
+        int n = 3;
         int[] arr = new int[n+1];
         arr[0]=0;
         arr[1]=1;
