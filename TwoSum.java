c9cc006c9b4fb9282f90b61633a96576976991c2
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-01-22 20:22:12 +0530

    Sum of Elements that Reflect with index

--- TwoSum.java
+++ TwoSum.java
@@ -18,8 +18,8 @@ class Solution {
     }
 
     public static void main(String[] args){
-        int[] nums = {2,7,11,15,20};
-        int target = 17;
+        int[] nums = {2,7,11,15,20,35};
+        int target = 20;
         int[] result = twoSum(nums, target);
         System.out.println(Arrays.toString(result));
     }
