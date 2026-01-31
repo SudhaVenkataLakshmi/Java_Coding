1ebdb1019ada525a1738e2a582ba6b11c436942f
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-01-31 21:34:40 +0530

    Counts Frequent Elements in List

--- FrequencyCount.java
+++ FrequencyCount.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class FrequencyCount {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(3,4,3,2,2,2,2,1,1,3,3,5,5,5,5,6,6,6,8,8,9,6,5,4,6,6);
+        List<Integer> numbers = Arrays.asList(3,4,3,2,2,2,2,1,1,3,3,5,5,5,5,6,6,6,8,8,9,6,5,4,6,6,8,8,8,9,8);
 
         Map<Integer, Integer> freqMap = new HashMap<>();
 
