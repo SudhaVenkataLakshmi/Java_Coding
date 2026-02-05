c79210fd709cb9fd81f80f856b4f91d7f032b727
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-05 19:50:24 +0530

    Design Triangle using #

--- StarTriangle.java
+++ StarTriangle.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class StarTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no.of steps:");
+        System.out.println("Enter no.of Rows:");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("# ");
+                System.out.print("* ");
             }
             System.out.println();
         }
