f2ff31773520de90018356350c37989be612c17f
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-05 19:38:02 +0530

    Merge Sorted Arrays and Display

--- MergeSortedArrays.java
+++ MergeSortedArrays.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class MergeSortedArrays {
     public static void main(String [] args){
         int arr1 [] = {1,2,4,9,12,15};
-        int arr2 [] = {3,5,7,10};
+        int arr2 [] = {3,5,7,10,6,8,14};
         int n1 = arr1.length;
         int n2 = arr2.length;
         int merged [] = new int[n1 + n2];
