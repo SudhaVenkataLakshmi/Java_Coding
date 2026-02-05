d98eb869783801144ee4732e36911be5f6ca7cc1
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-05 19:49:05 +0530

    Design Triangle using #

--- StarTriangle.java
+++ StarTriangle.java
@@ -8,7 +8,7 @@ public class StarTriangle {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print("# ");
             }
             System.out.println();
         }
