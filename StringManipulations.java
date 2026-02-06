f4908f4be509485711d7de0fdcf550196f34af59
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-06 14:50:00 +0530

    String Manipulations with exaples

--- StringManipulations.java
+++ StringManipulations.java
@@ -16,10 +16,17 @@ public static void main(String[] args){
 //     System.out.println(str1.replace('a', 'o'));
 
 
-String s1 = "Hello";
-String s2 = "Hello";
-String s3 = "HELLO";
-System.out.println(s1.equals(s2));
-System.out.println(s1.equalsIgnoreCase(s3));
+// String s1 = "Hello";
+// String s2 = "Hello";
+// String s3 = "HELLO";
+// System.out.println(s1.equals(s2));
+// System.out.println(s1.equalsIgnoreCase(s3));
+
+
+String str = "I love Coding in Java";
+System.out.println(str.contains("Java"));
   }
 }
+
+
+
