080cf37d276506f34cada3c5689b0e503702307a
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-06 14:15:53 +0530

    Finding Length of String

--- StringManipulations.java
+++ StringManipulations.java
@@ -1,9 +1,13 @@
 public class StringManipulations {
 public static void main(String[] args){
-    String s = "Sushma";
-    String name = "hello everyone!";
-    System.out.println(s.length());
-    System.out.println(s.toLowerCase());
-    System.out.println(name.toUpperCase());
+    // String s = "Sushma";
+    // String name = "hello everyone!";
+    // System.out.println(s.length());
+    // System.out.println(s.toLowerCase());
+    // System.out.println(name.toUpperCase());
+
+    String str1 = "Hello";
+    String str2 = "Guys..";
+    System.out.println(str1.concat(str2));
   }
 }
