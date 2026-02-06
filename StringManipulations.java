1d877895d97b181359b0e1f91e247cccac9c63d1
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-06 14:39:37 +0530

    String Manipulations with exaples

--- StringManipulations.java
+++ StringManipulations.java
@@ -6,13 +6,20 @@ public static void main(String[] args){
     // System.out.println(s.toLowerCase());
     // System.out.println(name.toUpperCase());
 
-    String str1 = "Java ";
-    String str2 = "Programing ";
-    String str3 = "Language";
-    System.out.println(str1.concat(str3));
+//     String str1 = "Java ";
+//     String str2 = "Programing ";
+//     String str3 = "Language";
+//     System.out.println(str1.concat(str3));
     
-    System.out.println(str2.charAt(5));
-    System.out.println(str2.substring(0,7));
-    System.out.println(str1.replace('a', 'o'));
+//     System.out.println(str2.charAt(5));
+//     System.out.println(str2.substring(0,7));
+//     System.out.println(str1.replace('a', 'o'));
+
+
+String s1 = "Hello";
+String s2 = "Hello";
+String s3 = "HELLO";
+System.out.println(s1.equals(s2));
+System.out.println(s1.equalsIgnoreCase(s3));
   }
 }
