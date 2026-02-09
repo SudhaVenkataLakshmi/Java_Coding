9d692c2908eea35cda35bbad20de0d291f388f03
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-09 19:14:34 +0530

    Checks Given String is Palindrome or Not

---
+++ PalindromeEx.java
@@ -0,0 +1,13 @@
+public class PalindromeEx {
+    public static void main(String[] args){
+        String str = "MadaM";
+        String rev = "";
+        for(int i = str.length()-1; i>=0; i--){
+            rev = rev + str.charAt(i);
+        }
+        if(str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
