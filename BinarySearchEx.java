cd727228cd91c7ec5c6b129a32c3f102c989d0a7
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-12 21:43:25 +0530

    Search Elements from Array

--- BinarySearchEx.java
+++ BinarySearchEx.java
@@ -23,7 +23,7 @@ public class BinarySearchEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int arr[] = {12,9,5,7,3,2,4,8,6,9,11,10};
+        int arr[] = {12,9,5,7,3,2,4,8,6,9,11,10,12,15,20,25};
         Arrays.sort(arr); 
 
         System.out.println("Sorted Array: " + Arrays.toString(arr));
