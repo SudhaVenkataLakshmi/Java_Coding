cbb4d66c4863ea9349b3217a2cbee16751ee9a62
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-15 18:54:47 +0530

    Checks Anagram or not

--- Anagram.java
+++ Anagram.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 public class Anagram {
     public static void main(String[] args){
-        String s1 = "listen";
-        String s2 = "silent";
+        String s1 = "silent";
+        String s2 = "listen";
 
         char[] arr1 = s1.toCharArray();
         char[] arr2 = s2.toCharArray();
