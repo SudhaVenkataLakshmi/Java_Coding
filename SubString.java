929ddb21d979d634dace22a4a17d9b6692e82d10
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-15 19:09:47 +0530

    Prints a part of String

---
+++ SubString.java
@@ -0,0 +1,10 @@
+public class SubString {
+    public static void main(String[] args){
+        String s = "abc";
+        for(int i = 0; i < s.length(); i++){
+            for(int j = 0; j <= s.length(); j++){
+                System.out.println(s.substring(i, j));
+            }
+        }
+    }
+}
