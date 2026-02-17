f4aeebb2a70dbfeb9e067c78956344a8ea4c3ef0
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 11:52:51 +0530

    Break Statement usage

--- Break.java
+++ Break.java
@@ -1,7 +1,7 @@
 public class Break {
     public static void main(String[] args){
-    for(int i = 0; i <= 10; i++){
-        if(i==5){
+    for(int i = 1; i <= 10; i++){
+        if(i==7){
             break;
         }
         System.out.println(i);
