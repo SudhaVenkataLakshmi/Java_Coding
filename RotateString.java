b8592d1e9a60230bf84ef316e8d006998ac5c0d7
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 15:15:19 +0530

    Rotates the Given String

---
+++ RotateString.java
@@ -0,0 +1,9 @@
+public class RotateString {
+    public static void main(String[] args){
+        String s = "abcde";
+        int n = 2;
+        String part1 = s.substring(n);
+        String part2 = s.substring(0,n);
+        System.out.println(part1 + part2);
+    }
+}
