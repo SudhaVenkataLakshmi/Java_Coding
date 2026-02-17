c0378093f0e0d903b0e6d87547aaf58d89d3c073
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 12:27:28 +0530

    Calculate Factorial Value

--- Fact.java
+++ Fact.java
@@ -1,6 +1,6 @@
 public class Fact {
     public static void main(String[] args){
-        int num = 5;
+        int num = 7;
         int fact = 1;
         for(int i = 1; i <= num; i++){
             fact = fact * i;
