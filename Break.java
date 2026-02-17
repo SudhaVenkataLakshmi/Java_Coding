76bb0b0e433dd9608eaac54e285ec1d3f9fffd09
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 12:30:26 +0530

    Breat Statement Example

--- Break.java
+++ Break.java
@@ -1,7 +1,7 @@
 public class Break {
     public static void main(String[] args){
     for(int i = 1; i <= 10; i++){
-        if(i==8){
+        if(i==5){
             break;
         }
         System.out.println(i);
