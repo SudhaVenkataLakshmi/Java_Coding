07630558abd959a9ec692663d357f0549c2e96e0
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 12:33:54 +0530

    Task on continue

--- Task.java
+++ Task.java
@@ -1,7 +1,7 @@
 public class Task {
     public static void main(String[] args){
         for(int i = 1; i <= 10; i++){
-            if(i % 2 != 0){
+            if(i % 2 == 0){
                 continue;
             }
             System.out.println(i);
