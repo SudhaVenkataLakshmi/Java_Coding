3b76d494a8977af80ff5b50e6a5d28d604da94e6
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 12:07:41 +0530

    Prints Numbers in Specific order

--- Task.java
+++ Task.java
@@ -1,7 +1,7 @@
 public class Task {
     public static void main(String[] args){
-        for(int i = 0; i <= 10; i++){
-            if(i % 2 == 0){
+        for(int i = 1; i <= 10; i++){
+            if(i % 2 != 0){
                 continue;
             }
             System.out.println(i);
