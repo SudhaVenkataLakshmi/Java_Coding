9c44f52144cef5cea6497023e472929ab37b3bc5
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 12:02:18 +0530

    Continue Statement usage

--- Continue.java
+++ Continue.java
@@ -1,7 +1,7 @@
 public class Continue {
     public static void main(String[] args){
-        for(int i = 0; i <= 10; i++){
-            if(i == 5){
+        for(int i = 0; i <= 5; i++){
+            if(i == 3){
                 continue;
             }
             System.out.println(i);
