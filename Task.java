8eeceff28d467d2b074f8ed0d80d5b98e473fb41
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-17 12:40:31 +0530

    Task

--- Task.java
+++ Task.java
@@ -1,10 +1,22 @@
-public class Task {
+// public class Task {
+//     public static void main(String[] args){
+//         for(int i = 1; i <= 10; i++){
+//             if(i % 2 == 0){
+//                 continue;
+//             }
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+public class Task{
     public static void main(String[] args){
+        int sum = 0;
         for(int i = 1; i <= 10; i++){
-            if(i % 2 == 0){
-                continue;
-            }
-            System.out.println(i);
+            sum = sum + i;
         }
+        System.out.println("Sum of 10 numbers: " +sum);
     }
 }
