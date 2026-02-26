93bcf8fff83d206209911d2b9921b6d0a85defdf
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-26 15:16:08 +0530

    Longest SubString length

--- LongestSubString.java
+++ LongestSubString.java
@@ -1,6 +1,6 @@
 public class LongestSubString {
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "xyzxyzxy";
         int maxLength = 0;
 
         for(int i = 0; i < s.length(); i++) {
