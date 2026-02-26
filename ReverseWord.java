3506142f57fa58756a140f7cf7e6e7f10311bd69
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-26 15:09:46 +0530

    Reversing each word in given string and display

--- ReverseWord.java
+++ ReverseWord.java
@@ -1,6 +1,6 @@
 public class ReverseWord {
     public static void main(String[] args){
-        String s = "Hello People!";
+        String s = "Hello Guys!";
         String[] words = s.split(" ");
         for(String word : words){
             String rev = "";
