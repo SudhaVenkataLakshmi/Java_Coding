f756a755a68f288ea1e83aa4cf861fbeb471d3f6
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-26 15:14:26 +0530

    Break.java

--- Break.java
+++ Break.java
@@ -1,7 +1,7 @@
 public class Break {
     public static void main(String[] args){
-    for(int i = 1; i <= 10; i++){
-        if(i==5){
+    for(int i = 1; i <= 5; i++){
+        if(i==2){
             break;
         }
         System.out.println(i);
