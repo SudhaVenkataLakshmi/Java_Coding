4a90421eec3e2faed677024fe99c1909f3dc9428
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-26 15:12:41 +0530

    Examples on Continue

--- Continue.java
+++ Continue.java
@@ -1,6 +1,6 @@
 public class Continue {
     public static void main(String[] args){
-        for(int i = 0; i <= 7; i++){
+        for(int i = 0; i <= 10; i++){
             if(i == 5){
                 continue;
             }
