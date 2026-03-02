14cf03922a977abe05fafb7c6cc0527f257e69b0
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-02 12:43:16 +0530

    Reverse a Number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ReverseNumber{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any Number: ");
         int n = sc.nextInt();
         int rev = 0;
         while(n > 0){
