465e5c46f5ef8ab1cc6178117d56307ac9a409cb
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-03 17:08:14 +0530

    Practice on Basice

--- Practice.java
+++ Practice.java
@@ -328,26 +328,59 @@
 // }
 
 
+// import java.util.Scanner;
+
+// public class Practice{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter size of Array: ");
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         System.out.println("Enter " +n+ " Elements: ");
+//         for(int i = 0; i < n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int min = arr[0];
+//         for(int i = 1; i < n; i++){
+//             if(arr[i] < min){
+//                 min = arr[i];
+//             }
+//         }
+//         System.out.println("Max Number: "+min);
+//     }
+// }
+
+
+
+
+
+// String Methods: 
+
+// length()   ---------------------------- to find the length of String
+
+// charAt(index)  ------------------------ trace the character at specific index
+
+// substring(start, end) ----------------- 
+
+// toUpperCase(), toLowerCase() ---------- converts lower to uppercase and upper to lowercase characters in given string
+
+// equals() ------------------------------ 
+
+// equalsIgnoreCase() --------------------
+
+// contains() ----------------------------
+
+// indexOf() -----------------------------
+
+// replace() -----------------------------
+
 import java.util.Scanner;
 
 public class Practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of Array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter " +n+ " Elements: ");
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
-        }
-        int min = arr[0];
-        for(int i = 1; i < n; i++){
-            if(arr[i] < min){
-                min = arr[i];
-            }
-        }
-        System.out.println("Max Number: "+min);
+        System.out.println("Enter any String: ");
+        String name = sc.nextLine();
+        System.out.println("Length: "+name.length());
     }
 }
-
-
