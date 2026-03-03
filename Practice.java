7b4d9a4230931d7ef00dfd0ca9aff7ff1c4f7439
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-03 17:31:14 +0530

    Practice on Basice

--- Practice.java
+++ Practice.java
@@ -387,20 +387,35 @@
 //     }
 // }
 
+// import java.util.Scanner;
+
+// public class Practice{
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter any String: ");
+//         String str = sc.nextLine().toLowerCase();
+//         int count = 0;
+//         for(int i = 0; i < str.length(); i++){
+//             char ch = str.charAt(i);
+//             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+//                 count++;
+//             }
+//         }
+//         System.out.println("Total Vowels: "+count);
+//     }
+// }
+
 import java.util.Scanner;
 
 public class Practice{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any String: ");
-        String str = sc.nextLine().toLowerCase();
-        int count = 0;
-        for(int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                count++;
-            }
+        String str = sc.nextLine();
+        String rev = "";
+        for(int i = str.length()-1; i>=0; i--){
+            rev = rev + str.charAt(i);
         }
-        System.out.println("Total Vowels: "+count);
+        System.out.println("reversed: " +rev);
     }
 }
