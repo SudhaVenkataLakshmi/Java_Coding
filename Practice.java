d4bc69b8b8a4488abfc414d749e7e04be3a52a79
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-03 16:53:01 +0530

    Practice on Basice

--- Practice.java
+++ Practice.java
@@ -305,6 +305,29 @@
 //     }
 // }
 
+// import java.util.Scanner;
+
+// public class Practice{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter size of Array: ");
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         System.out.println("Enter " +n+ " Elements: ");
+//         for(int i = 0; i < n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int max = arr[0];
+//         for(int i = 1; i < n; i++){
+//             if(arr[i] > max){
+//                 max = arr[i];
+//             }
+//         }
+//         System.out.println("Max Number: "+max);
+//     }
+// }
+
+
 import java.util.Scanner;
 
 public class Practice{
@@ -317,13 +340,13 @@ public class Practice{
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        int max = arr[0];
+        int min = arr[0];
         for(int i = 1; i < n; i++){
-            if(arr[i] > max){
-                max = arr[i];
+            if(arr[i] < min){
+                min = arr[i];
             }
         }
-        System.out.println("Max Number: "+max);
+        System.out.println("Max Number: "+min);
     }
 }
 
