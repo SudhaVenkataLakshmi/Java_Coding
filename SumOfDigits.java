96591e7e624d3cfb64fb10479c1dbd401a8ad876
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-07 20:30:53 +0530

    Finds Sum of Digits in Given Number

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class SumOfDigits{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any Number: ");
+        System.out.println("Enter any Number you want: ");
         int n = sc.nextInt();
         int sum = 0;
         while(n > 0){
             sum += n % 10;
             n /= 10;
         }
-        System.out.println("Sum of Digits: " +sum);
+        System.out.println("Sum of Digits in your Number: " +sum);
     }
 }
