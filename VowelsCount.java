4ea6ccc8b9816e9a7eef8fc81da766fb12f02a61
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-14 21:21:13 +0530

    Counts Vowels in String

--- VowelsCount.java
+++ VowelsCount.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class VowelsCount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any String: ");
+        System.out.println("Enter any String you want to check: ");
         String str = sc.nextLine();
         int count = 0;
         for(int i = 0; i < str.length(); i++){
