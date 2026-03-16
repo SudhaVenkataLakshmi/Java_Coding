515140635b56752590ed81172f3c412330c911d5
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-16 12:27:04 +0530

    Sum of Maximum Sub Array Elements

---
+++ MaxSubArraySum.java
@@ -0,0 +1,20 @@
+public class MaxSubArraySum {
+
+    public static void main(String[] args) {
+        int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(currentSum + arr[i] > arr[i]){
+                currentSum = currentSum + arr[i];
+            }
+            else{
+                currentSum = arr[i];
+            }
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+            }
+        }
+        System.out.println("Maximum Sub Array Sum: " +maxSum);
+    }
+}
