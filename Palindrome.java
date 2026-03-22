935d82086a745fd93473c8865f0ee957e5ad7618
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-22 20:06:26 +0530

    Checks Given String is Palindrome or not

--- Palindrome.java
+++ Palindrome.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Palindrome{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your String: ");
+        System.out.println("Enter any String: ");
         String str = sc.nextLine();
         String rev = "";
         for(int i = str.length()-1; i >= 0; i--){
             rev += str.charAt(i);
         }
         if(str.equals(rev)){
-            System.out.println("This is Palindrome String");
+            System.out.println("This is Palindrome");
         }else{
-            System.out.println("This is not a Palindrome String");
+            System.out.println("This is not a Palindrome");
         }
         sc.close();
     }
