64f1610bea122d0f0ee538ec9b19824b7677cf0e
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-03-22 20:16:31 +0530

    finds missing number

--- missingNumber.java
+++ missingNumber.java
@@ -1,13 +1,13 @@
 public class missingNumber {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,5};
-        int N = 5;
+        int arr[] = {1,2,3,5,6,7};
+        int N = 7;
         int expectedSum = N * (N + 1) / 2;
         int actualSum = 0;
         for(int i = 0; i < arr.length; i++){
             actualSum = actualSum + arr[i];
         }
         int missingNumber = expectedSum - actualSum;
-        System.out.println("Missing Number: " +missingNumber);
+        System.out.println("Missing Number is: " +missingNumber);
     }
 }
