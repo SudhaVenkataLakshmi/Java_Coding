d4ed7b3458527317f87584a0c8129b22600d9685
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-04-03 17:08:56 +0530

    Checks the number Anagram or not

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class ValidAnagram {
     public static void main(String[] args) {
-        String s = "brag";
-        String t = "grab";
+        String s = "listen";
+        String t = "silent";
         char[] arr1 = s.toCharArray();
         char[] arr2 = t.toCharArray();
         Arrays.sort(arr1);
