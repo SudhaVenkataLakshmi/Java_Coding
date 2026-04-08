4c12a7f0d55cae1e1f3ba02edc62c23228d42b00
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-04-08 19:59:20 +0530

    Sum of Maximum SubArray

--- MaxSubArraySum.java
+++ MaxSubArraySum.java
@@ -1,7 +1,7 @@
 public class MaxSubArraySum {
 
     public static void main(String[] args) {
-        int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int arr[] = {-2, 1, -3, 4, -1, 2, 1, -4, 6,7};
         int maxSum = arr[0];
         int currentSum = arr[0];
         for(int i = 0; i < arr.length; i++){
@@ -15,6 +15,6 @@ public class MaxSubArraySum {
                 maxSum = currentSum;
             }
         }
-        System.out.println("Maximum Sub Array Sum: " +maxSum);
+        System.out.println("Maximum Sub Array Sum is : " +maxSum);
     }
 }
