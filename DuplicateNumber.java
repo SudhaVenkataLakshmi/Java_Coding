b9870e3096e27ca9832ad9cb956ebc74ef8790c1
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-16 19:22:43 +0530

    Finds Duplicate numbers

--- DuplicateNumber.java
+++ DuplicateNumber.java
@@ -1,6 +1,6 @@
 public class DuplicateNumber {
     public static void main(String[] args) {
-        int arr[] = {1, 3, 4, 2, 2};
+        int arr[] = {1, 3, 4, 2, 2, 5, 2};
         for(int i = 0; i < arr.length; i++){
             for(int j = i+1; j < arr.length; j++){
                 if(arr[i] == arr[j]){
@@ -10,3 +10,5 @@ public class DuplicateNumber {
         }
     }
 }
+
+
