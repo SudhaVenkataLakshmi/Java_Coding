f4591e295a456b7f0a0d043b88f957586222d81d
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-23 21:05:27 +0530

    Practice on Arrays

--- ArraysPractice.java
+++ ArraysPractice.java
@@ -1,12 +1,12 @@
 public class ArraysPractice {
     public static void main(String[] args) {
-        int[] arr = {10, 50, 30, 90, 20};
-        int max = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+        int[] array = {2,6,9,10,12,30,9};
+        int max = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(array[i] > max){
+                max = array[i];
             }
         }
-        System.out.println("Maximun Element: " +max);
+        System.out.println("Maximum Value: " +max);
     }
 }
