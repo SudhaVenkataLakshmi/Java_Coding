68eee5bcbc64e4052b974da4da186a541624cd71
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-24 20:02:56 +0530

    Sum of Elements in Array

---
+++ Arrays3.java
@@ -0,0 +1,10 @@
+public class Arrays3 {
+    public static void main(String[] args) {
+        int[] arr3 = {3,6,8,9,10,2};
+        int sum = 0;
+        for(int i = 0; i < arr3.length; i++){
+            sum += arr3[i];
+        }
+        System.out.println(sum);
+    }
+}
