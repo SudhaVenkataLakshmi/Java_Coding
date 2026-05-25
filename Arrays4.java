d46b796a0e73e14e06d539089bf413e26da0eecb
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-25 14:46:07 +0530

    Find the Minimum Element from Array

--- Arrays4.java
+++ Arrays4.java
@@ -1,6 +1,6 @@
 public class Arrays4{
     public static void main(String[] args) {
-        int[] arr = {20,40,10,50};
+        int[] arr = {15,10,9,4,7,18,30};
         int min = arr[0];
         for(int i = 0; i < arr.length; i++){
             if(arr[i] < min){
