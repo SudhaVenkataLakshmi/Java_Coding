69f1ab4ebe76565f92bb32a8b1c3e6a6dd027cca
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-25 14:55:44 +0530

    Count Even and Odd Numbers in Given Array

---
+++ Arrays6.java
@@ -0,0 +1,16 @@
+public class Arrays6 {
+    public static void main(String[] args) {
+        int[] arr = {12,5,6,8,11,3,10,16,5,3,18,20};
+        int even = 0;
+        int odd = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 == 0){
+                even ++;
+            }else {
+                odd ++;
+            }
+        }
+        System.out.println("No. of Even Numvers: " +even);
+        System.out.println("No. of Odd Numbers: " +odd);
+    }
+}
