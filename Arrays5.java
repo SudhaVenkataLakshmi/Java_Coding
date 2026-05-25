f1be0551976bc4566b2497fec46ddedbeeb3a56a
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-25 14:49:37 +0530

    Print Even numbers from given array

--- Arrays5.java
+++ Arrays5.java
@@ -1,6 +1,6 @@
 public class Arrays5 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8};
+        int[] arr = {11,7,9,14,12,90,18};
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % 2 == 0){
                 System.out.println(arr[i]);
