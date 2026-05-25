e164db787c589407021ddd0ded4b77e3fa62ac39
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-25 15:07:57 +0530

    Copy one Arrays into Another Array

---
+++ Arrays8.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Arrays8 {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,5};
+        int [] arr2 = new int[arr1.length];
+        for(int i = 0; i < arr1.length; i++){
+            arr2[i] = arr1[i];
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
+}
