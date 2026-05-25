3a7b9d1a00dd72d3261504497def374ffb2969ca
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-25 15:27:12 +0530

    Copy One Array Elements into Another array

--- Arrays8.java
+++ Arrays8.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Arrays8 {
     public static void main(String[] args) {
-        int [] arr1 = {6,8,3,2,5,9};
+        int [] arr1 = {7,8,3,5,2,1};
         int [] arr2 = new int[arr1.length];
         for(int i = 0; i < arr1.length; i++){
             arr2[i] = arr1[i];
