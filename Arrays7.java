f876215560ee60dd2aa6bb34862487a83e8334bf
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-25 15:28:12 +0530

    Finds Average of Elements in Array

--- Arrays7.java
+++ Arrays7.java
@@ -1,6 +1,6 @@
 public class Arrays7 {
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50,60};
+        int[] arr = {100,50,150,25,175};
         int sum = 0;
         for(int i = 0 ; i < arr.length; i++){
             sum += arr[i];
