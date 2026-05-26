9bdc215ee8d4999b20602b9ce64092ab2009be43
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-26 19:57:06 +0530

    Finding an Element from an Array

--- Arrays2.java
+++ Arrays2.java
@@ -1,6 +1,6 @@
 public class Arrays2{
     public static void main(String[] args) {
-        int[] array = {12,9,5,3,8,11,18};
+        int[] array = {10,6,3,8,9,12,15,20,30,25};
         int search = 9;
         boolean found = false;
         for(int i = 0; i < array.length; i++){
