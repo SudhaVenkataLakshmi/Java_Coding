3e1e8e5da51187e75384ca081693928d9aef4a7d
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-26 19:55:02 +0530

    Reversing an Array and Print

--- Arrays1.java
+++ Arrays1.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Arrays1{
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1,2,3,4,8,9,10,12};
         int start = 0;
         int end = arr.length - 1;
         while(start < end){
