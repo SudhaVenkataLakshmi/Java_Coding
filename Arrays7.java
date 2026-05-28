c9c8226bdca1877aea3dd4dad257512f392fc246
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-28 20:44:24 +0530

    Calculates Average of All elements in Array and print

--- Arrays7.java
+++ Arrays7.java
@@ -1,6 +1,6 @@
 public class Arrays7 {
     public static void main(String[] args) {
-        int[] arr = {100,50,150,25,175};
+        int[] arr = {15,30,25,42,19,9};
         int sum = 0;
         for(int i = 0 ; i < arr.length; i++){
             sum += arr[i];
