dd7515c853bd08a3a698b12f33a127fcf6fc0156
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-28 20:34:45 +0530

    Finds minimum element from Array

--- Arrays4.java
+++ Arrays4.java
@@ -1,6 +1,6 @@
 public class Arrays4{
     public static void main(String[] args) {
-        int[] arr = {15,10,9,4,7,18,30};
+        int[] arr = {15,10,9,12,5,20,2,7};
         int min = arr[0];
         for(int i = 0; i < arr.length; i++){
             if(arr[i] < min){
