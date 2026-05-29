738ef3820bfb68ea24fdd38a0c96efddd2947e7a
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-29 21:16:49 +0530

    Converting given String into character

--- String1.java
+++ String1.java
@@ -1,9 +1,9 @@
 public class String1 {
     public static void main(String[] args) {
-        String str = "Sushma";
-        char[] ch = str.toCharArray();
-        for(int i = 0; i < ch.length; i++){
-            System.out.println(ch[i]);
+        String str = "Practice";
+        char[] c = str.toCharArray();
+        for(int i = 0; i < c.length; i++){
+            System.out.println(c[i]);
         }
     }
 }
