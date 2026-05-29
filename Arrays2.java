7f1ba71444755126109a3999d0df6f74c25886c7
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-29 21:32:32 +0530

    Finds specific element from given Array

--- Arrays2.java
+++ Arrays2.java
@@ -1,6 +1,6 @@
 public class Arrays2{
     public static void main(String[] args) {
-        int[] array = {10,6,3,8,9,12,15,20,30,25};
+        int[] array = {3,6,8,9,10,2,15,12,5};
         int search = 9;
         boolean found = false;
         for(int i = 0; i < array.length; i++){
