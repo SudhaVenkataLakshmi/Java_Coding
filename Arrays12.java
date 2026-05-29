2caf53d4b5ec1f0917b3154961ee7edfd1c4a16f
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-29 20:57:02 +0530

    find duplicate elements from Array

---
+++ Arrays12.java
@@ -0,0 +1,12 @@
+public class Arrays12 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,2,4,1};
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+}
