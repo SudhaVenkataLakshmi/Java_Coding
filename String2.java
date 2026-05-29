d11adcec851e04a27e642ce45ea3d5469b2a4cc2
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-29 21:21:48 +0530

    Reverse a String

---
+++ String2.java
@@ -0,0 +1,8 @@
+public class String2 {
+    public static void main(String[] args) {
+        String str = "Java";
+        for(int i = str.length()-1; i >= 0; i--){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
