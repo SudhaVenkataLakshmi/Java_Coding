90f580273fbcfac4824add620cba930e20b24c73
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-06-07 21:00:27 +0530

    Count consonants in given string

---
+++ String5.java
@@ -0,0 +1,21 @@
+public class String5 {
+     public static void main(String[] args) {
+
+        String str = "Java";
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if(Character.isLetter(ch) &&
+              !(ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u')) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
