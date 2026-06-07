7cf7c49c64fdc8491407d6d6b617e79a46c30fe5
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-06-07 21:06:07 +0530

    Search an element

--- Arrays2.java
+++ Arrays2.java
@@ -1,6 +1,6 @@
 public class Arrays2{
     public static void main(String[] args) {
-        int[] array = {3,6,8,9,10,2,4,12,10,7,20,16,19};
+        int[] array = {3,4,7,9,12,10,3,1};
         int search = 20;
         boolean found = false;
         for(int i = 0; i < array.length; i++){
