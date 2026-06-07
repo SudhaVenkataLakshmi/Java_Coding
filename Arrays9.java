482fea220af9ad52f7c892d7a954c0dbae4b20eb
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-06-07 21:05:20 +0530

    Gives second largest element in array

--- Arrays9.java
+++ Arrays9.java
@@ -1,6 +1,6 @@
 public class Arrays9 {
     public static void main(String[] args) {
-        int[] arr = {8,9,4,5,2,12,10,6,16};
+        int[] arr = {8,9,4,5,2,12,10,6,1};
         int largest = Integer.MIN_VALUE;
         int secLargest = Integer.MIN_VALUE;
         for(int i = 0; i < arr.length; i++) {
