361a84120a80d7b774cdecd5562099146284798e
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-06-26 11:17:02 +0530

    Finds secod largest element

--- Arrays9.java
+++ Arrays9.java
@@ -1,6 +1,6 @@
 public class Arrays9 {
     public static void main(String[] args) {
-        int[] arr = {8,9,4,5,2,12,10,6,1};
+        int[] arr = {12,6,9,10,4,3,10,7};
         int largest = Integer.MIN_VALUE;
         int secLargest = Integer.MIN_VALUE;
         for(int i = 0; i < arr.length; i++) {
