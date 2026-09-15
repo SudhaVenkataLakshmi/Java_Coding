8bfa5290edd99c3b37c799bd18b8fcf0d4d35842
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-09-15 19:55:23 +0530

    Arrays searching

--- Arrays2.java
+++ Arrays2.java
@@ -1,6 +1,6 @@
 public class Arrays2{
     public static void main(String[] args) {
-        int[] array = {12,5,7,9,10,2,4,15};
+        int[] array = {12,5,7,9,10,2,4,15,20,18,11,3};
         int search = 20;
         boolean found = false;
         for(int i = 0; i < array.length; i++){
