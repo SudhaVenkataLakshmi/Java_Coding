140521817a82865348def5df2660d8493e1083a4
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-09-15 19:45:41 +0530

    Checks Palindrome

--- String3.java
+++ String3.java
@@ -1,6 +1,6 @@
 public class String3 {
     public static void main(String[] args) {
-        String str = "abba";
+        String str = "allaaaa";
         String rev = "";
         for(int i  = str.length() - 1; i >= 0; i--){
             rev += str.charAt(i);
