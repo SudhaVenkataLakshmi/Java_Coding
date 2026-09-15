5643bf0b9de66a8a87f53ce4437cd3358c725cdc
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-09-15 19:53:40 +0530

    Arrays Practice

--- Arrays1.java
+++ Arrays1.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Arrays1{
     public static void main(String[] args) {
-        int[] arr = {10,40,5,3,12};
+        int[] arr = {10,40,5,3,12,11,20,15,18};
         int start = 0;
         int end = arr.length - 1;
         while(start < end){
